0ac42b508567bf11f3bc3871a498ddeb7f70ad70
Andrew Liu <andrew.charles.liu@gmail.com>
2016-01-16 15:48:27 -0500

    Updated default address to target TransLoc location

--- app/src/main/java/com/andrewcl/ipinfodbandroidclient/MapsActivity.java
+++ app/src/main/java/com/andrewcl/ipinfodbandroidclient/MapsActivity.java
@@ -3,10 +3,12 @@ package com.andrewcl.ipinfodbandroidclient;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -38,9 +40,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker to TransLoc headquarters and move the camera
+        LatLng transloc = new LatLng(35.875208, -78.840620);
+        mMap.addMarker(new MarkerOptions().position(transloc).title("Marker at TransLoc"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(transloc));
+
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(transloc).zoom(12).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
