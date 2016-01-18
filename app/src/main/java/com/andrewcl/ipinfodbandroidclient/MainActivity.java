c4ad7ae10c4b01de55048f3678af2642095b6386
Andrew Liu <andrew.charles.liu@gmail.com>
2016-01-17 22:08:43 -0500

    ip address handling over range

--- app/src/main/java/com/andrewcl/ipinfodbandroidclient/MainActivity.java
+++ app/src/main/java/com/andrewcl/ipinfodbandroidclient/MainActivity.java
@@ -9,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import java.lang.reflect.Array;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -43,5 +46,59 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(launchMapsIntent);
             }
         });
+
+
+        List<String> testStringArray = stringArrayFromRange("127.0.0.1", "127.0.0.6");
+        System.out.println("YOYOYOYOY: " + Integer.toString(testStringArray.size()));
+    }
+
+    private List<String> stringArrayFromRange(String startIPAddressRange, String stopIPAddressRange) {
+        int startInteger = convertIPAddressStringToInt(startIPAddressRange);
+        int stopInteger = convertIPAddressStringToInt(stopIPAddressRange);
+
+        //Switches order in case user has reversed ip address range fields
+        if (startInteger < stopInteger){
+            int newStopInt = startInteger;
+            startInteger = stopInteger;
+            stopInteger = newStopInt;
+        }
+
+        List<String> ipAddressesArray = new ArrayList<>();
+        while (startInteger <= stopInteger) {
+            InetAddress inetAddress = ipIntToInetAddress(startInteger);
+            ipAddressesArray.add(inetAddress.toString());
+            startInteger += 1;
+        }
+        return ipAddressesArray;
+    }
+
+    public InetAddress ipIntToInetAddress(int ipAsInt) {
+        byte[] ipAsByteArray = intToByteArray(ipAsInt);
+        try {
+            InetAddress inetAddress = InetAddress.getByAddress(ipAsByteArray);
+            return inetAddress;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static byte[] intToByteArray(int a) {
+        byte[] ret = new byte[4];
+        ret[3] = (byte) (a & 0xFF);
+        ret[2] = (byte) ((a >> 8) & 0xFF);
+        ret[1] = (byte) ((a >> 16) & 0xFF);
+        ret[0] = (byte) ((a >> 24) & 0xFF);
+        return ret;
+    }
+
+    public static int convertIPAddressStringToInt(String ipAddressString) {
+        String[] ipAddressInArray = ipAddressString.split("\\.");
+        int ipAddress = 0;
+        for (int i = 0; i < ipAddressInArray.length; i++) {
+            int ipComponentAsInt = Integer.parseInt(ipAddressInArray[i]);
+            ipAddress += ipComponentAsInt * Math.pow(256, 3 - i);
+        }
+        return ipAddress;
     }
 }
